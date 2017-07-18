5f1ae21f376dc78a0e0a0184df6cc83bf99e762f
MarcoASC4 <marcoag2261@gmail.com>
2017-07-18 11:00:13 -0400

    IDK

---
+++ p5Projects/marco-typetest/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+    createCanvas(500, 300);
+    background(0, 255, 127)
+}
+
+function draw() {
+    fill(0, 102, 153);
+    text("Please type the sentences below.", 150, 50);
+}
